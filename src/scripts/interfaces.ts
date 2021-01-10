07c50314802dcf020932aab1c0e2c337dc5d451c
Gaisar Davletkildin <Davletkildin.G.S@sberbank.ru>
2021-01-11 01:36:53 +0300

    Тестовое задание - Frontend Developer (Travelpayouts)

--- src/scripts/interfaces.ts
+++ src/scripts/interfaces.ts
@@ -1,8 +1,8 @@
 export interface IOptions{
-  widgetId: string;
-  textColor: string;
-  buttonBg: string;
-  widgetBg: string;
+  widgetId?: string;
+  textColor?: string;
+  buttonBg?: string;
+  widgetBg?: string;
 }
 
 export interface IWidget{
